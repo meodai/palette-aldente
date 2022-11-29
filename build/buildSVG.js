f30c0aa6c98dc4a935c46bdfdd029f5798c74641
David Aerne <meodai@gmail.com>
2022-11-29 21:55:39 +0100

    update generated svg

--- build/buildSVG.js
+++ build/buildSVG.js
@@ -5,9 +5,10 @@ import fs from 'fs';
 const palettes = JSON.parse(fs.readFileSync('./dist/palettes.json', 'utf8'));
 const items = palettes.length;
 const w = 200;
-const itemH = 30;
+const itemH = 10;
 const padding = 5;
-const fontSize = 12;
+const inlineSpace = 2;
+const fontSize = 6;
 const h = ( fontSize + itemH + padding * 2 ) * items;
 
 
@@ -20,10 +21,10 @@ const svg = `
       }
     </style>
     ${palettes.map((c, i) => {
-      const width = w/c.colors.length;
+      const width = w/c.colors.length - inlineSpace;
       const top = fontSize + fontSize/1.5 + i * (itemH + fontSize + padding * 2);
-      return `<text y="${top - fontSize + fontSize/1.5}" x="3">${c.name}</text>` + c.colors.map((hex, j) => {
-        return `<rect width="${width}" height="${itemH}" stroke="#212121" stroke-width="1" y="${top}" x="${j * width}" fill="${hex}" />`
+      return `<text y="${top - fontSize + fontSize/1.4}" x="3">${c.name}</text>` + c.colors.map((hex, j) => {
+        return `<rect width="${width}" height="${itemH}" y="${top}" x="${j * width + j * inlineSpace}" fill="${hex}" />`
       }).join('\n');
     }).join('\n')}
   </svg>
