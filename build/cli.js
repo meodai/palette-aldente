1bcb26b7ea31564901e6b7687a2d4d9f4913d755
David Aerne <meodai@gmail.com>
2022-11-30 14:39:13 +0100

    make sure CLI creates directories if inexistant

--- build/cli.js
+++ build/cli.js
@@ -2,10 +2,12 @@
 import { program } from 'commander';
 import fs from 'fs';
 import path from 'path';
-import {fileURLToPath} from 'url';
 
+/*
+import {fileURLToPath} from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+*/
 
 import {
   possibleConverters,
