afd3d1a7bed4e1b35e0c8b39c8dc7466ca806fa9
David Aerne <meodai@gmail.com>
2022-11-30 14:34:16 +0100

    make sure CLI creates directories if inexistant

--- build/cli.js
+++ build/cli.js
@@ -2,6 +2,10 @@
 import { program } from 'commander';
 import fs from 'fs';
 import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 import {
   possibleConverters,
@@ -69,8 +73,16 @@ program
         defaultColorFormat, 
         additionalColorFormats
       );
+      
+      if (!fs.existsSync(options.out)){
+        fs.mkdirSync(options.out, { recursive: true });
+      }
+
       fs.writeFileSync(
-        path.join(options.out, 'palettes.json'), 
+        path.join(
+          options.out, 
+          'palettes.json'
+        ), 
         JSON.stringify(
           paletteArray,
           null,
